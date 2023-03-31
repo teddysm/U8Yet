f709ed5919f97c96fed109cad0cc9f4c5b3a238b
Trevor McGill <trevor.mcgill@icloud.com>
2023-03-30 21:27:17 -0500

    little update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,8 @@
 var nashville = {lat:36.1627, lng:-86.7816}
 
-// This example adds a search box to a map, using the Google Place Autocomplete
+// Adds a search box to a map, using the Google Place Autocomplete
 // feature. People can enter geographical searches. The search box will return a
 // pick list containing a mix of places and predicted search terms.
-// This example requires the Places library. Include the libraries=places
-// parameter when you first load the API. For example:
-// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
 function initAutocomplete() {
   const map = new google.maps.Map(document.getElementById("map"), {
     center: nashville,
